979968a78dc6310c513fb7317d0d9eb7a7782500
MouhamadKa <mouhamad.akasem@gmail.com>
2025-07-02 15:03:42 +0400

    Fix some bugs in the user managment process.

--- website/views.py
+++ website/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-
 from django.contrib import messages 
+from .forms import SignUpForm
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -10,10 +11,11 @@ from django.contrib import messages
 
 def home(request):
     if request.method == 'POST':
-        email = request.POST['email']
-        password = request.POST['password']    
+        username = request.POST['username']
+        password = request.POST['password']
+        print(username, password)
 
-        user = authenticate(request, email=email, password=password)
+        user = authenticate(username=username, password=password)
         if user:
             login(request, user)
             messages.success(request, "You are logged in!")
@@ -31,5 +33,20 @@ def logout_user(request):
     return redirect('home')
 
 
-def register_user(request):
-    return render(request, 'register.html', {})
+def register_user(request):    
+	if request.method == 'POST':
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			# Authenticate and login
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			messages.success(request, "You Have Successfully Registered! Welcome!")
+			return redirect('home')
+	else:
+		form = SignUpForm()
+		return render(request, 'register.html', {'form':form})
+
+	return render(request, 'register.html', {'form':form})
